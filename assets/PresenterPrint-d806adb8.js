bc44673babaa30942f0fb6dd7ee0d15ce173c576
Nguyen Gia Huy <huynguyen6349@gmail.com>
2023-10-27 13:46:07 +0000

    Add more content

--- assets/PresenterPrint-d806adb8.js
+++ assets/PresenterPrint-d806adb8.js
@@ -1,4 +1,4 @@
-import{d as _,u as d,a as p,c as m,b as h,r as u,o as a,e as n,f as t,t as s,g as l,F as f,h as g,n as v,i as x,j as y,k as b,l as k,m as N,_ as w}from"./index-f5ae6d9c.js";import{N as P}from"./NoteDisplay-446ee04f.js";const V={class:"m-4"},L={class:"mb-10"},S={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},B={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},j=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},C=_({__name:"PresenterPrint",setup(F){d(`
+import{d as _,u as d,a as p,c as m,b as h,r as u,o as a,e as n,f as t,t as s,g as l,F as f,h as g,n as v,i as x,j as y,k as b,l as k,m as N,_ as w}from"./index-f3fac00a.js";import{N as P}from"./NoteDisplay-408cdd63.js";const V={class:"m-4"},L={class:"mb-10"},S={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},B={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},j=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},C=_({__name:"PresenterPrint",setup(F){d(`
 @page {
   size: A4;
   margin-top: 1.5cm;
